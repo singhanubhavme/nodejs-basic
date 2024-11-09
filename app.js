9c94da8cf6f14607865fc925fc19ae92aba6da9d
Anubhav Singh <singhanubhav58@gmail.com>
2024-11-09 22:10:10 +0530

    added redis

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./db.config.js";
 import userModel from "./user.model.js";
+import { createClient } from "redis";
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
 async function bootstrap() {
   await connectDB();
+  await redisClient.connect();
+
   const app = express();
 
   app.use(express.json());
@@ -17,12 +26,30 @@ async function bootstrap() {
   app.post("/users", async (req, res) => {
     const { name } = req.body;
     const user = await userModel.create({ name });
+    await redisClient.del("users");
     res.status(201).json(user);
   });
 
   app.get("/users", async (req, res) => {
-    const users = await userModel.find();
-    res.status(200).json(users);
+    try {
+      const cachedUsers = await redisClient.get("users");
+
+      if (cachedUsers) {
+        console.log("Fetching users from cache");
+        return res.status(200).json(JSON.parse(cachedUsers));
+      }
+
+      const users = await userModel.find();
+      await redisClient.set("users", JSON.stringify(users), {
+        EX: 3600,
+      });
+      console.log("Fetching users from Database");
+
+      res.status(200).json(users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
   });
 
   app.listen(process.env.PORT, () => {
@@ -30,4 +57,7 @@ async function bootstrap() {
   });
 }
 
-bootstrap();
+bootstrap().catch((error) => {
+  console.error("Error starting the server:", error);
+  redisClient.quit();
+});
